c20938c38ca51fc0999f91662c7cf3207e57d567
Brett Jurman <i.be.brett@gmail.com>
2022-12-22 17:37:53 -0500

    add options for building native vs wasm

--- build.zig
+++ build.zig
@@ -1,51 +1,50 @@
 const std = @import("std");
 
 pub fn build(b: *std.build.Builder) void {
-    // Standard target options allows the person running `zig build` to choose
-    // what target to build for. Here we do not override the defaults, which
-    // means any target is allowed, and the default is native. Other options
-    // for restricting supported target set are available.
-
     const target = b.standardTargetOptions(.{});
-
-    const installSprites = b.addInstallFile(.{ .path = "assets/sprites.png" }, "bin/sprites.png");
-
-    // Standard release options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
     const mode = b.standardReleaseOptions();
 
-    const exe = b.addExecutable("game1", "src/main.zig");
-    exe.addLibraryPath("SDL2-devel-2.26.1-VC/SDL2-2.26.1/lib/x64");
-    exe.addIncludePath("SDL2-devel-2.26.1-VC/SDL2-2.26.1/include");
+    const build_native = b.option(bool, "native", "Build the native executable.") orelse false;
+    const build_wasm = b.option(bool, "wasm", "Build the wasm library.") orelse false;
 
-    exe.addLibraryPath("SDL2_image-devel-2.6.2-VC/SDL2_image-2.6.2/lib/x64");
-    exe.addIncludePath("SDL2_image-devel-2.6.2-VC/SDL2_image-2.6.2/include");
+    if (build_native) {
+        const installSprites = b.addInstallFile(.{ .path = "assets/sprites.png" }, "bin/sprites.png");
 
-    const installSDLImageDLL = b.addInstallFile(.{ .path = "SDL2_image-devel-2.6.2-VC/SDL2_image-2.6.2/lib/x64/SDL2_image.dll" }, "bin/SDL2_image.dll");
-    const installSDLDLL = b.addInstallFile(.{ .path = "SDL2-devel-2.26.1-VC/SDL2-2.26.1/lib/x64/SDL2.dll" }, "bin/SDL2.dll");
+        // Standard release options allow the person running `zig build` to select
+        // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
+        const exe = b.addExecutable("game1", "src/main.zig");
 
-    exe.linkLibC();
-    exe.linkSystemLibraryName("SDL2");
-    exe.linkSystemLibraryName("SDL2_image");
+        exe.addLibraryPath("SDL2-devel-2.26.1-VC/SDL2-2.26.1/lib/x64");
+        exe.addIncludePath("SDL2-devel-2.26.1-VC/SDL2-2.26.1/include");
+        exe.addLibraryPath("SDL2_image-devel-2.6.2-VC/SDL2_image-2.6.2/lib/x64");
+        exe.addIncludePath("SDL2_image-devel-2.6.2-VC/SDL2_image-2.6.2/include");
 
-    exe.setTarget(target);
-    exe.setBuildMode(mode);
-    exe.install();
+        const installSDLImageDLL = b.addInstallFile(.{ .path = "SDL2_image-devel-2.6.2-VC/SDL2_image-2.6.2/lib/x64/SDL2_image.dll" }, "bin/SDL2_image.dll");
+        const installSDLDLL = b.addInstallFile(.{ .path = "SDL2-devel-2.26.1-VC/SDL2-2.26.1/lib/x64/SDL2.dll" }, "bin/SDL2.dll");
 
-    exe.step.dependOn(&installSDLImageDLL.step);
-    exe.step.dependOn(&installSDLDLL.step);
-    exe.step.dependOn(&installSprites.step);
+        exe.linkLibC();
+        exe.linkSystemLibraryName("SDL2");
+        exe.linkSystemLibraryName("SDL2_image");
 
-    const exe_tests = b.addTest("src/main.zig");
-    exe_tests.setTarget(target);
-    exe_tests.setBuildMode(mode);
+        exe.setTarget(target);
+        exe.setBuildMode(mode);
+
+        exe.install();
+
+        exe.step.dependOn(&installSDLImageDLL.step);
+        exe.step.dependOn(&installSDLDLL.step);
+        exe.step.dependOn(&installSprites.step);
+    }
 
     // add a step for wasm
-    const wasm = b.addSharedLibrary("wasm", "src/main_wasm.zig", .unversioned);
-    wasm.setTarget(.{ .cpu_arch = .wasm32, .os_tag = .freestanding });
-    wasm.setBuildMode(.ReleaseFast);
-    wasm.install();
+    if (build_wasm) {
+        const wasm = b.addSharedLibrary("wasm", "src/main_wasm.zig", .unversioned);
 
-    const wasm_step = b.step("wasm", "build wasm");
-    wasm_step.dependOn(&wasm.step);
+        wasm.setTarget(.{ .cpu_arch = .wasm32, .os_tag = .freestanding });
+        wasm.setBuildMode(.ReleaseFast);
+
+        wasm.install();
+        const wasm_step = b.step("wasm", "build wasm");
+        wasm_step.dependOn(&wasm.step);
+    }
 }
